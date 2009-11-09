89571631a5dcc0b5a020364bf71ad39cb1b20f2f
j.otten@hudora.de <j.otten@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-11-09 14:19:17 +0000

    disabled one test. increased startup waiting time and added a resting periodf before shutdown
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/pyJasper/trunk@6983 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- pyjasper/backend/tests/test.sh
+++ pyjasper/backend/tests/test.sh
@@ -5,9 +5,11 @@ BASEPATH=$(dirname $0)
 
 cd $BASEPATH
 
+PYJASPER_SERVLET_URL="http://localhost:4580/pyJasper/jasper.py"
+
 FAULT=0
 test_pdf() {
-    file $(PYTHONPATH=. $PYTHON client.py $@ | grep writing | awk '{ print $3 }') | grep "PDF document"	
+    file $(PYTHONPATH=. $PYTHON client.py --url="$PYJASPER_SERVLET_URL" $@ | grep writing | awk '{ print $3 }') | grep "PDF document"	
     returnval=$?
     echo $FAULT
     if [ $returnval -ne 0 ]; then 
@@ -17,18 +19,20 @@ test_pdf() {
     return $returnval
 }
 
-PYJASPER_SERVLET_URL="http://localhost:4580/pyJasper/jasper.py"
 
 #start server:
-../pyJasper-httpd.sh -Djetty.port=4580 &
-PID=$!
+../pyJasper-httpd.sh -Djetty.port=4580 -DSTOP.PORT=8079 &
 
 # high sleep is needed for jetty to process the .jar files the first time
-sleep 15
+sleep 45
 
 test_pdf bestellanlage.jrxml bestellanlage-subreport1.jrxml bestellanlage-subreport2.jrxml bestellanlage-subreport3.jrxml 
 
+# give it some more time
+sleep 5
+
 # jetty builtin stop mechanism
 java -DSTOP.KEY=blaat -DSTOP.PORT=8079 -jar ../start.jar --stop
+
 
 exit $FAULT
